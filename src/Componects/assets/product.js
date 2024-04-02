812e524424b6aeb9266e271b0db15cdf29bbc692
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2024-04-02 23:53:03 +0530

    product_image upload

---
+++ src/Componects/assets/product.js
@@ -0,0 +1,187 @@
+import pic_1 from "./men/pic_1.jpg";
+import pic_2 from "./men/pic_2.jpg";
+import pic_3 from "./men/pic_3.jpg";
+import pic_4 from "./men/pic_4.jpg";
+import pic_5 from "./men/pic_5.jpg";
+import pic_6 from "./men/pic_6.jpg";
+import pic_7 from "./men/pic_7.jpg";
+import pic_8 from "./men/pic_8.jpg";
+import pic_9 from "./men/pic_9.jpg";
+import pic_10 from "./men/pic_10.jpg";
+import pic_11 from "./men/pic_11.jpg";
+import pic_12 from "./men/pic_12.jpg";
+import pic_13 from "./kids/pic_13.jpg";
+import pic_14 from "./kid/pic_14.jpg";
+import pic_15 from "./kid/pic_15.jpg";
+import pic_16 from "./kid/pic_16.jpg";
+import pic_17 from "./kid/pic_17.jpg";
+import pic_18 from "./kid/pic_18.jpg";
+import pic_19 from "./kid/pic_19.jpg";
+import pic_20 from "./kid/pic_20.jpg";
+import pic_21 from "./kid/pic_21.jpg";
+import pic_22 from "./kid/pic_22.jpg";
+import pic_23 from "./kid/pic_23.jpg";
+import pic_24 from "./kid/pic_24.jpg";
+import pic_25 from "./kid/pic_25.jpg";
+import pic_26 from "./kid/pic_26.jpg";
+import pic_27 from "./kid/pic_27.jpg";
+import pic_28 from "./women/pic_28.jpg";
+import pic_29 from "./women/pic_29.jpg";
+import pic_30 from "./women/pic_30.jpg";
+import pic_31 from "./women/pic_31.jpg";
+import pic_32 from "./women/pic_32.jpg";
+import pic_33 from "./women/pic_33.jpg";
+import pic_34 from "./women/pic_34.jpg";
+import pic_35 from "./women/pic_35.jpg";
+import pic_36 from "./women/pic_36.jpg";
+import pic_37 from "./women/pic_37.jpg";
+import pic_38 from "./women/pic_38.jpg";
+import pic_39 from "./women/pic_39.jpg";
+
+let all_product = [
+  {
+    id: 1,
+    name: "Black half shirt ",
+    category: "men",
+    image: pic_1,
+    new_price: 499,
+    old_price: 1299,
+  },
+  {
+    id: 2,
+    name: "half red t shirt",
+    category: "men",
+    image: pic_2,
+    new_price: 599,
+    old_price: 1499,
+  },
+  {
+    id: 3,
+    name: "white t shirt",
+    category: "men",
+    image: pic_3,
+    new_price: 499,
+    old_price: 1299,
+  },
+  {
+    id: 4,
+    name: "neon green t shirt flag football flag",
+    category: "men",
+    image: pic_4,
+    new_price: 999,
+    old_price: 1999,
+  },
+  {
+    id: 5,
+    name: "mountain rough grey t shirt  ",
+    category: "men",
+    image: pic_5,
+    new_price: 499,
+    old_price: 1299,
+  },
+  {
+    id: 6,
+    name: "grey hiphop t shirt ",
+    category: "men",
+    image: pic_6,
+    new_price: 499,
+    old_price: 1299,
+  },
+  {
+    id: 7,
+    name: "t shirt ",
+    category: "men",
+    image: pic_7,
+    new_price: 899,
+    old_price: 1399,
+  },
+  {
+    id: ,
+    name: "t shirt ",
+    category: "men",
+    image: pic_1,
+    new_price: 499,
+    old_price: 1299,
+  },
+  {
+    id: ,
+    name: "t shirt ",
+    category: "men",
+    image: pic_1,
+    new_price: 499,
+    old_price: 1299,
+  },
+  {
+    id: ,
+    name: "t shirt ",
+    category: "men",
+    image: pic_1,
+    new_price: 499,
+    old_price: 1299,
+  },
+  {
+    id: ,
+    name: "t shirt ",
+    category: "men",
+    image: pic_1,
+    new_price: 499,
+    old_price: 1299,
+  },
+  {
+    id: ,
+    name: "t shirt ",
+    category: "men",
+    image: pic_1,
+    new_price: 499,
+    old_price: 1299,
+  },
+  {
+    id: ,
+    name: "t shirt ",
+    category: "men",
+    image: pic_1,
+    new_price: 499,
+    old_price: 1299,
+  },
+
+  {
+    id: ,
+    name: "t shirt ",
+    category: "men",
+    image: pic_1,
+    new_price: 499,
+    old_price: 1299,
+  },
+  {
+    id: ,
+    name: "t shirt ",
+    category: "men",
+    image: pic_1,
+    new_price: 499,
+    old_price: 1299,
+  },
+  {
+    id: ,
+    name: "t shirt ",
+    category: "men",
+    image: pic_1,
+    new_price: 499,
+    old_price: 1299,
+  },
+  {
+    id: ,
+    name: "t shirt ",
+    category: "men",
+    image: pic_1,
+    new_price: 499,
+    old_price: 1299,
+  },
+  {
+    id: ,
+    name: "t shirt ",
+    category: "men",
+    image: pic_1,
+    new_price: 499,
+    old_price: 1299,
+  },
+];
