a01a7c7c2f5cd4b19d3e94ddbd3033ddb2ab0eb4
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2024-04-07 02:18:40 +0530

    hero complete

--- src/Componects/Hero/Hero.jsx
+++ src/Componects/Hero/Hero.jsx
@@ -8,14 +8,15 @@ const Hero = () => {
   return (
     <div className="hero">
       <div className="hero-left">
-        <h2>NEW ARRIVALS ONLY</h2>
+        <h2>NEW ARRIVALS</h2>
         <div>
           <div className="hero-hand-icon">
             <p>Hey</p>
             <img src={hero_wave_icon} alt="sale_icon" />
-            <p>Get Your </p>
-            <p>Latest Collection </p>
           </div>
+          <p>Get Your </p>
+          <p>Latest Collection </p>
+
           <div className="hero-latest-btn">
             <div>Discount Upto 70%</div>
             <img src={sale_icon} alt="sale icon two"></img>
---
+++ src/Componects/Item/Item.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Item.css";
+
+const Item = () => {
+  return <div className="item">Item</div>;
+};
+export default Item;
