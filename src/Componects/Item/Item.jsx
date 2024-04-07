d2e896bce0b1729e9c4d64fb140208f20943aee1
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2024-04-07 17:38:46 +0530

    small error was showing

--- src/Componects/Item/Item.jsx
+++ src/Componects/Item/Item.jsx
@@ -7,7 +7,7 @@ const Item = (props) => {
       <img src={props.image} style={{ width: "300px" }} alt="" />
       <p>{props.name}</p>
       <div className="item-prices">
-        <div className="item-price-new">{props.price_new}</div>s
+        <div className="item-price-new">{props.price_new}</div>
         <div className="item-price-old">{props.price_old}</div>
       </div>
     </div>
