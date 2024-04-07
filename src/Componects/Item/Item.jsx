ce5698f119c5bf5b49d07f48c6331e7ccb4a15cd
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2024-04-07 15:13:31 +0530

    item css completed

--- src/Componects/Item/Item.jsx
+++ src/Componects/Item/Item.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import "./Item.css";
 
-const Item = () => {
-  return <div className="item">Item</div>;
+const Item = (props) => {
+  return (
+    <div className="item">
+      <img src={props.image} alt="" />
+      <p>{props.name}</p>
+      <div className="item-prices">
+        <div className="item-price-new">{props.price_new}</div>s
+        <div className="item-price-old">{props.price_old}</div>
+      </div>
+    </div>
+  );
 };
 export default Item;
