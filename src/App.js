67b7756cc970fa160fe7596ad611763feca22f27
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2024-04-08 23:36:23 +0530

    soloved the rendering problem

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Shop from "./Pages/Shop";
 import ShopCategory from "./Pages/ShopCategory";
 import Product from "./Pages/Product";
 import LoginSignUp from "./Pages/LoginSignUp";
+import Cart from "./Pages/Cart";
 
 function App() {
   return (
@@ -13,13 +14,22 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />}></Route>
-          <Route path="/mens"element={<ShopCategory category="mens" />}></Route>
-          <Route path="/womens" element={<ShopCategory category="womens" />} ></Route>
-          <Route path="/kids" element={<ShopCategory category="kids" />} ></Route>
+          <Route
+            path="/mens"
+            element={<ShopCategory category="mens" />}
+          ></Route>
+          <Route
+            path="/womens"
+            element={<ShopCategory category="womens" />}
+          ></Route>
+          <Route
+            path="/kids"
+            element={<ShopCategory category="kids" />}
+          ></Route>
           <Route path="/product" element={<product />}>
             <Route path=":productId" element={<Product />} />
           </Route>
-          <Route path="/cart" element={<cart />} />
+          <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<LoginSignUp />} />
         </Routes>
       </BrowserRouter>
--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -7,7 +7,6 @@ const Shop = () => {
     <div>
       <Hero />
       <Popular />
-      <Shop />
     </div>
   );
 };
