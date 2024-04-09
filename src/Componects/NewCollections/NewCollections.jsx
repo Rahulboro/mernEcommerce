a4d3b7b77f18dd7e60f4b15c939d05fb00c82ff6
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2024-04-09 19:39:12 +0530

    starting newcollections

---
+++ src/Componects/NewCollections/NewCollections.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./NewCollections.css";
+
+const NewCollections = () => {
+  return (
+    <div className="New-collections">
+      <h1>NEW COLLECTIONS</h1>
+      <hr />
+      <div className="collections"></div>
+    </div>
+  );
+};
+
+export default NewCollections;
