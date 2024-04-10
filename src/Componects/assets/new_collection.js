53c3c588fc7ba4adcb79f5ed4104d5d69d31a5f6
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2024-04-10 17:08:23 +0530

    new collection complete

--- src/Componects/assets/new_collection.js
+++ src/Componects/assets/new_collection.js
@@ -2,6 +2,10 @@ import newColl_1 from "../assets/new-collection/newColl_1.jpg";
 import newColl_2 from "../assets/new-collection/newColl_2.jpg";
 import newColl_3 from "../assets/new-collection/newColl_3.jpg";
 import newColl_4 from "../assets/new-collection/newColl_4.jpg";
+import newColl_5 from "../assets/new-collection/newColl_5.jpg";
+import newColl_6 from "../assets/new-collection/newColl_6.jpg";
+import newColl_7 from "../assets/new-collection/newColl_7.jpg";
+import newColl_8 from "../assets/new-collection/newColl_8.jpg";
 
 let New_Collection_Data = [
   {
@@ -15,7 +19,7 @@ let New_Collection_Data = [
   {
     id: 2,
     name: "new collection red christian with white collar girl dress",
-    category: "men",
+    category: "girl",
     image: newColl_2,
     new_price: 599,
     old_price: 1499,
@@ -23,7 +27,7 @@ let New_Collection_Data = [
   {
     id: 3,
     name: "new collection white  blouse for women with black pants",
-    category: "men",
+    category: "girl",
     image: newColl_3,
     new_price: 499,
     old_price: 1299,
@@ -31,10 +35,42 @@ let New_Collection_Data = [
   {
     id: 4,
     name: "new collection women indian  print salwar kameez",
-    category: "men",
+    category: "girl",
     image: newColl_4,
     new_price: 999,
     old_price: 1999,
+  },
+  {
+    id: 5,
+    name: "new collection women indian attire red with chumki lehenga choli",
+    category: "girl",
+    image: newColl_5,
+    new_price: 999,
+    old_price: 1999,
+  },
+  {
+    id: 6,
+    name: "new collection women black and white stripe single top",
+    category: "girl",
+    image: newColl_6,
+    new_price: 999,
+    old_price: 1999,
+  },
+  {
+    id: 7,
+    name: "new collection women red top with black spot desgin",
+    category: "girl",
+    image: newColl_7,
+    new_price: 999,
+    old_price: 1999,
+  },
+  {
+    id: 8,
+    name: "new collection women white dress with flower desgin",
+    category: "girl",
+    image: newColl_8,
+    new_price: 1799,
+    old_price: 2599,
   },
 ];
 
