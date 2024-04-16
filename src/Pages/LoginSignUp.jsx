1f98c53b34b5dc102db3229c7e632a5f87343db9
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2024-04-16 23:44:41 +0530

    "Add basic structure for LoginSignUp page"
    Explanation:
    The code diff shows the addition of a basic structure for the LoginSignUp page. This includes a container with a form for user input, a continue button, a

--- src/Pages/LoginSignUp.jsx
+++ src/Pages/LoginSignUp.jsx
@@ -1,6 +1,27 @@
 import React from "react";
 
 const LoginSignUp = () => {
-  return <div></div>;
+  return (
+    <div className="loginSignup">
+      <div className="loginSignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginSignup-form">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Your Email" />
+          <input type="password" placeholder="Your password" />
+        </div>
+        <button className="login-btn">Continue</button>
+        <div className="loginSignup-login">
+          <p>
+            Already have Account <span>Login Here</span>
+          </p>
+        </div>
+        <div className="loginSignup-agree">
+          <input type="checkbox" name="" id=""></input>
+          <p>By Continuing, i agree to the terms of use & privacy policy.</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default LoginSignUp;
