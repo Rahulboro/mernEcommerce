3fdd47677607e1e25f822485b70c44e4c7b9eb2a
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2024-04-17 23:45:46 +0530

    breadcrum componects are made

---
+++ src/Componects/BreadCrums/BreadCrum.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./BreadCrum.css";
+
+const BreadCrums = (props) => {
+  const { product } = props;
+  return <div className="breadCrum">Home</div>;
+};
+
+export default BreadCrums;
