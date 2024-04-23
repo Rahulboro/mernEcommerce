e98cbf89d3f560c00ea16e43790753e0416b26fa
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2024-04-24 00:16:46 +0530

    "Convert ProductDisplay to accept a product prop and render product details"
    This commit updates the `ProductDisplay` component to accept a `product` prop and renders the product's details, including the product's name, image,

--- src/Componects/ProductDisplay/ProductDisplay.jsx
+++ src/Componects/ProductDisplay/ProductDisplay.jsx
@@ -1,8 +1,34 @@
 import React from "react";
 import "./productdisplay.css";
 
-const ProductDisplay = () => {
-  return <div className="product-display">Product Display</div>;
+const ProductDisplay = (props) => {
+  const { product } = props;
+  return (
+    <div className="product-display">
+      <div className="productdisplay-left">
+        <div className="productdisplay-image-list">
+          <img src={product} alt="" />
+          <img src={product} alt="" />
+          <img src={product} alt="" />
+          <img src={product} alt="" />
+          <img src={product} alt="" />
+        </div>
+        <div className="productdisplay-image">
+          <img className="productdisplay-main-img" src={product.image} />
+        </div>
+      </div>
+      <div className="productdisplay-right">
+        <h1>{product.name}</h1>
+        <div className="productdisplay-right-star">
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDisplay;
