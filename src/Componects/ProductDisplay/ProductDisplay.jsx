66ffcc9ec0bb23c78a12d440efa4741269455f89
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2024-04-25 22:46:51 +0530

    "Updated ProductDisplay component to display product image and details; added star ratings and product description"
    This commit updates the ProductDisplay component to correctly display the product image and adds various details such as product name, star ratings, old and new prices

--- src/Componects/ProductDisplay/ProductDisplay.jsx
+++ src/Componects/ProductDisplay/ProductDisplay.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./productdisplay.css";
+import star_icon from "../assets/icons/star-solid-24.png";
+import star_dull from "../assets/icons/star-regular-24.png";
 
 const ProductDisplay = (props) => {
   const { product } = props;
@@ -7,11 +9,11 @@ const ProductDisplay = (props) => {
     <div className="product-display">
       <div className="productdisplay-left">
         <div className="productdisplay-image-list">
-          <img src={product} alt="" />
-          <img src={product} alt="" />
-          <img src={product} alt="" />
-          <img src={product} alt="" />
-          <img src={product} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
         </div>
         <div className="productdisplay-image">
           <img className="productdisplay-main-img" src={product.image} />
@@ -20,11 +22,43 @@ const ProductDisplay = (props) => {
       <div className="productdisplay-right">
         <h1>{product.name}</h1>
         <div className="productdisplay-right-star">
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_dull} alt="" />
+          <p>(154)</p>
+        </div>
+        <div className="productdisplay-right-prices">
+          <div className="productdisplay-right-price-old">
+            ${product.old_price}
+          </div>
+          <div className="productdisplay-right-price-old">
+            ${product.new_price}
+          </div>
+          <div className="productdisplay-right-descripttion">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio minus
+            odit ab illo quis harum! Fugiat aut maxime veritatis iusto facilis,
+            eos enim, assumenda itaque sapiente debitis perferendis et
+            temporibus!.
+          </div>
+          <div className="productdisplay-right-size">
+            <h1>Size:</h1>
+            <div className="ProductDisplay-right-size">
+              <div className="">S</div>
+              <div className="">M</div>
+              <div className="">L</div>
+              <div className="">XL</div>
+              <div className="">XXL</div>
+            </div>
+          </div>
+          <button>ADD TO CART</button>
+          <p className="productdisplay-right-category">
+            <span>Category:</span>Women, T-Shirt, Crop-top
+          </p>
+          <p className="productdisplay-right-category">
+            <span>tags:</span>Modern, Latest
+          </p>
         </div>
       </div>
     </div>
