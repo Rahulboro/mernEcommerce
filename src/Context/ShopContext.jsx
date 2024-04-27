ae3a0a2b17226a8e665c76ace457b165a914985a
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2024-04-28 01:45:58 +0530

    "Added state and method to ShopContextProvider; replaced null default value of ShopContext with initial cart state and all\_product"

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -2,9 +2,21 @@ import React, { createContext } from "react";
 import all_product from "../Componects/assets/product";
 
 export const ShopContext = createContext(null);
+const getDefault = () => {
+  let cart = {};
+  for (let index = 0; index < all_product.length + 1; index++) {
+    cart[index] = 0;
+  }
+  return cart;
+};
 const ShopContextProvider = (props) => {
   //state
-  const contextValue = { all_product };
+  const [cartItems, setCartItems] = React.useState(getDefault());
+  const contextValue = { all_product, cartItems };
+  //methods
+  const addToCart = (ItemId) => {
+    setCartItems({ ...cartItems, [ItemId]: cartItems[ItemId] + 1 });
+  };
   return (
     <ShopContext.Provider value={contextValue}>
       {props.children}
